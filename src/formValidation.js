bd3184177091171eb03d82544937160b0fec268f
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-10 02:22:21 -0500

    set up project to implement JavaScript Validation API

---
+++ src/formValidation.js
@@ -0,0 +1,18 @@
+
+
+export default function formValidate(event) {
+
+    console.log("formValidate function");
+    event.preventDefault();
+
+    const formEmail = document.getElementById("email");
+    const formCountry = document.getElementById("country");
+    const formZipcode = document.getElementById("zip-code");
+    const formPass = document.getElementById("password");
+    const formConfirmPass = document.getElementById("password-confirm");
+
+
+    // JavaScript Validation API implementation
+    
+
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
+import formValidate from './formValidation';
 import './style.css';
 
+
+// the form's skeleton HTML
 const formHTML = `<form>
-<input type="email" name="email" id="email">
-<input type="text" name="country" id="country">
-<input type="tel" name="zip-code" id="zip-code">
-<input type="password" name="password" id="password">
-<input type="password" name="password-confirm" id="password-confirm">
+<input type="email" name="email" id="email" placeholder="Email">
+<input type="text" name="country" id="country" placeholder="Country">
+<input type="text" name="zip-code" id="zip-code" pattern="d{5}" placeholder="Zip Code">
+<input type="password" name="password" id="password" placeholder="Password">
+<input type="password" name="password-confirm" id="password-confirm" placeholder="Confirm Password">
+<button type="submit" id="submit-button">Submit</button>
 </form>`;
 
-const contentDiv = document.getElementById("content");
 
-contentDiv.innerHTML += formHTML;
+// render form HTML onto screen
+const contentDiv = document.getElementById("content");
+contentDiv.innerHTML = formHTML;
+
+
+// add click listener to form submit button for input validation checking
+const formSubmitButtton = document.getElementById("submit-button");
+formSubmitButtton.addEventListener("click", formValidate);
 
